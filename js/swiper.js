ca24e008087fde529495d188b011252867064e3f
Bogdanchik88 <bogdanchebotarev@gmail.com>
2024-05-15 23:07:13 +0300

    slider

--- js/swiper.js
+++ js/swiper.js
@@ -3,6 +3,8 @@ document.addEventListener("DOMContentLoaded", () => {
     if (window.innerWidth < 420) {
         const points = document.querySelectorAll(".point");
         points[0].classList.add("visible");
+        const predlozheniya = document.querySelectorAll(".banner")
+        predlozheniya[0].classList.add("visible-banner") 
     }
   });
 
@@ -33,4 +35,20 @@ function switchCard(id) {
     } else {
         console.log("123")
     }
+}
+
+function move(id) {
+    const block = document.querySelector(".selectors");
+    const oldButton = block.getElementsByClassName("selected_swiper")[0];
+    oldButton.classList.remove("selected_swiper");
+    const selectedButton = document.getElementById(id);
+    selectedButton.classList.add("selected_swiper");
+
+    const cardBlock = document.querySelector(".banners")
+    const oldCard = cardBlock.getElementsByClassName("visible-banner")[0];
+    oldCard.classList.remove("visible-banner");
+    newId = id.split("_")[1];
+    const newCard = document.getElementById("ban-"+ newId)
+    newCard.classList.add("visible-banner")
+
 }
