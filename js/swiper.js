891b5293db312d66a4ad077030e72c34bdcdb0d3
Bogdanchik88 <bogdanchebotarev@gmail.com>
2024-05-17 16:03:19 +0300

    ok

--- js/swiper.js
+++ js/swiper.js
@@ -8,35 +8,6 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-function switchCard(id) {
-    const cards = document.querySelector(".slider");
-
-    if (window.innerWidth < 420) {
-        const activeCard = cards.getElementsByClassName("visible")[0]
-        activeCard.classList.remove('visible');
-        const activeCardId = activeCard.id.split("-")[1];
-        const intId = parseInt(activeCardId);
-
-        const types = {
-            "left": (intId - 1),
-            "right": (intId + 1),
-        }
-
-        var newId = types[id];
-
-        if (newId > 3) {
-            newId = 1;
-        } else if (newId < 1) {
-            newId = 3;
-        }
-
-        const newCard = document.getElementById("point-" + (newId));
-        newCard.classList.add("visible");
-    } else {
-        console.log("123")
-    }
-}
-
 function move(id) {
     const block = document.querySelector(".selectors");
     const oldButton = block.getElementsByClassName("selected_swiper")[0];
@@ -49,6 +20,6 @@ function move(id) {
     oldCard.classList.remove("visible-banner");
     newId = id.split("_")[1];
     const newCard = document.getElementById("ban-"+ newId)
-    newCard.classList.add("visible-banner")
-
+    newCard.classList.add("visible-banner") 
 }
+
