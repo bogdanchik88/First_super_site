9574327cd2ebdaea26cbd6126af88e6b8b9bfb72
Bogdanchik88 <bogdanchebotarev@gmail.com>
2024-05-19 11:11:32 +0300

    fix

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,7 @@
 function redirectToUrl() {
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+    window.location.href = "https://first-super-site-git-sitepc-bogdanchik88s-projects.vercel.app/";
+})
