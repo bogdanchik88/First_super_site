193e1ca9286a0ece52ee7355a9a6e43c9e5dd2f2
Bogdanchik88 <bogdanchebotarev@gmail.com>
2024-05-19 11:01:34 +0300

    fix

--- js/redirect.js
+++ js/redirect.js
@@ -1,3 +1,11 @@
 function redirectToUrl() {
     window.location.href = "https://www.youtube.com/watch?v=dQw4w9WgXcQ";
 }
+
+window.addEventListener("resize", () => {
+    if (window.innerWidth < 420) {
+        window.location.href = "https://first-super-site-git-sitemobile-bogdanchik88s-projects.vercel.app/";
+    } else {
+        window.location.href = "https://first-super-site-git-sitepc-bogdanchik88s-projects.vercel.app/";
+    }
+});
